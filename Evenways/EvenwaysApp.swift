17c0c493cfb09a9b119b7036d68c98398609a82a
Diego Mendoza <diegocoronado_@outlook.com>
2026-05-02 10:43:56 -0700

    Initial Commit

---
+++ Evenways/EvenwaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvenwaysApp.swift
+//  Evenways
+//
+//  Created by Diego Mendoza on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct EvenwaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
